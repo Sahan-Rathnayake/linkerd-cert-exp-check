c95b2348ac7c0686bf89a5480b4953a2661abfad
Sahan-Rathnayake <smanjitha@gmail.com>
2025-05-08 01:00:27 +0530

    parameterize hard coded vars | move kubectl install to script

--- scripts/cert-exp-check.sh
+++ scripts/cert-exp-check.sh
@@ -3,26 +3,21 @@
 set -e
 set -x
 
-while getopts e:p:t: PARAMETER
-do
-	case "${PARAMETER}" in
-		e) ENVIRONMENT=${OPTARG};;
-		p) PROJECT=${OPTARG};;
-		t) TOPIC_ARN=${OPTARG};;
-	esac
-done
+curl -sLO https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl
+mv kubectl /usr/bin/kubectl
+chmod +x /usr/bin/kubectl
 
-kubectl get secret -n linkerd | grep "kubernetes.io/tls" | awk '{print $1}' > certificate-list.txt
+kubectl get secret -n ${LINKERD_NAMESPACE} | grep "kubernetes.io/tls" | awk '{print $1}' > certificate-list.txt
 
 while read CERTIFICATE
 do
-	read -ra DATE_ARR <<< `kubectl get secret "$CERTIFICATE" -n linkerd -o "jsonpath={.data['tls\.crt']}" | base64 -d | openssl x509 -enddate -noout | cut -d "=" -f 2-`
+	read -ra DATE_ARR <<< `kubectl get secret "$CERTIFICATE" -n ${LINKERD_NAMESPACE} -o "jsonpath={.data['tls\.crt']}" | base64 -d | openssl x509 -enddate -noout | cut -d "=" -f 2-`
 	echo "Expiration Date > ${DATE_ARR[@]}"
 	REMAINDER=`echo $(date -d "${DATE_ARR[0]} ${DATE_ARR[1]} ${DATE_ARR[3]}" +%s) $(date -d "${date}" +%s) | awk '{print ($1 - $2)/86400}'`
 	echo "Remaining Days > $REMAINDER"
 
-	if [ $REMAINDER -lt 45 ]
+	if [ $REMAINDER -lt 45 $THRESHOLD ]
 	then
-		aws sns publish --region "ap-southeast-1" --topic-arn "$TOPIC_ARN" --subject "Linkerd certificate expiration in $PROJECT-$ENVIRONMENT" --message "Linkerd certificate $CERTIFICATE in $PROJECT-$ENVIRONMENT will expire in $REMAINDER days"
+		aws sns publish --region $AWS_REGION --topic-arn $TOPIC_ARN --subject $SUBJECT --message $MESSAGE
 	fi
 done < certificate-list.txt
